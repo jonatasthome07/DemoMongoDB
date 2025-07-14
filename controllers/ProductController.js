9e3a8e1125f4a36148010f2535c5b1cfaed1aa44
Jônatas <jonatasthome04@gmail.com>
2025-07-14 13:56:09 -0300

    Route to save product and simple CSS was added

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -3,4 +3,19 @@ module.exports = class ProductController {
     static showProducts(req,res){
         res.render("products/all")
     }
+
+    static createProducts(req,res){
+        res.render("products/create")
+    }
+
+    static async createProductsPost(req,res){
+        try {
+           const {name, price, description} = req.body
+           const product = new Product(name, price, description)
+           product.save()
+           res.redirect("/products")
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const ProductController = require("../controllers/ProductController")
 
 router.get("/", ProductController.showProducts)
+router.get("/create", ProductController.createProducts)
+router.post("/create", ProductController.createProductsPost)
 
 module.exports = router
