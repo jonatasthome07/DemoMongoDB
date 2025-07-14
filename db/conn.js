f54d3b65bf1453dd96bdcea4dfef63d76d54071a
Jônatas <jonatasthome04@gmail.com>
2025-07-14 13:27:37 -0300

    MVC and first view was added

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,10 @@
+//Importa apenas o MongoClient do módulo para se conectar ao banco
 const {MongoClient} = require('mongodb')
 
+//URI para instanciar uma conexão localmente
 const uri = "mongodb://localhost:27017/demomongodb"
 
+//Instanciamos a classe com a URI da conexão
 const client = new MongoClient(uri)
 
 async function run() {
